ba10ac60522e580f074514aff3c6e4ec8b307fb5
Simon Pfeiffer <simon-p-96@web.de>
2021-08-26 19:01:08 +0200

    Adding SQL Alchemy

--- CuriousFeed.py
+++ CuriousFeed.py
@@ -1,6 +1,20 @@
+from enum import unique
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class Content(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(20), unique=True)
+    link = db.Column(db.String())
+    cover_image = db.Column(db.String(20), default='default.jpg')
+    category = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"Content('{self.title}', '{self.link}', '{self.cover_image}'"
 
 @app.route("/")
 def Home():
