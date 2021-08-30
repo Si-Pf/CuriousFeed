3447ae9928a804e043739d13fe6f0d83a35e5cb4
Simon Pfeiffer <simon-p-96@web.de>
2021-08-30 14:41:27 +0200

    modify css content

--- CuriousFeed.py
+++ CuriousFeed.py
@@ -25,4 +25,5 @@ def Video():
     return render_template('video.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    #app.run(host='0.0.0.0', port=3000) #Codesphere version
+    app.run(debug=True)
