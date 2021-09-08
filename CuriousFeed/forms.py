b57ac8baf93cdb1aea6ab0b1c336b85e94628b93
Simon Pfeiffer <simon-p-96@web.de>
2021-09-08 10:35:50 +0200

    Various additions

--- CuriousFeed/forms.py
+++ CuriousFeed/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, ValidationError
+from CuriousFeed.models import Content
+#import requests
 
 class SubmitMediaForm(FlaskForm):
     title = StringField('Title', 
@@ -11,3 +13,21 @@ class SubmitMediaForm(FlaskForm):
     link = StringField('Link', validators=[DataRequired()])
 
     submit = SubmitField('Submit your media')
+    
+    
+
+    def validate_link(self, link):
+        content = Content.query.filter_by(link = link.data).first()
+        if content:
+            raise ValidationError('Content with the same link has already been submitted')
+
+        #if category =="Video":
+          #  r = requests.get(link.data)
+          #  if "Video unavailable" in r.text:
+           #     raise ValidationError('Currently only Youtube video links are supported, the submitted link is not a valid Youtube video')
+
+    def validate_title(self, title):
+        content = Content.query.filter_by(title = title.data).first()
+        if content:
+            raise ValidationError('This title is already taken, please chose a different title')
+
--- CuriousFeed/routes.py
+++ CuriousFeed/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect
 from flask.helpers import url_for
-from CuriousFeed import app
+from urllib.parse import urlparse, parse_qs
+from CuriousFeed import app, db
 from CuriousFeed.models import Content
 from CuriousFeed.forms import SubmitMediaForm
 
@@ -10,7 +11,7 @@ def Home():
 
 @app.route("/video")
 def Video():
-    return render_template('video.html', video = Content.query.filter(Content.category == "Video").first())
+      return render_template('video.html', video = Content.query.filter(Content.category == "Video").first())
 
 @app.route("/book")
 def Book():
@@ -19,13 +20,39 @@ def Book():
 
 @app.route("/podcast")
 def Podcast():
-    return render_template('podcast.html')
+    return render_template('podcast.html', podcast = Content.query.filter(Content.category == "Podcast").first())
 
 
 @app.route("/submit_media", methods=['GET', 'POST'])
 def Submit():
     form = SubmitMediaForm()
+    # Extract Youtube ID from URL
+    def get_video_id(video):
+        u_pars = urlparse(video)
+        quer_v = parse_qs(u_pars.query).get('v')
+        if quer_v:
+            return quer_v[0]
+        pth = u_pars.path.split('/')
+        if pth:
+            return pth[-1]
+
+    def get_podcast_id(podcast):
+        id = podcast.rpartition('/')[-1]
+        return id
+
+
     if form.validate_on_submit():
+        if form.category.data =="Video":
+            content = Content(title = form.title.data, category = form.category.data, link = "http://www.youtube.com/embed/"+get_video_id(form.link.data)+"?modestbranding=1")
+
+        elif form.category.data =="Podcast":
+            content = Content(title = form.title.data, category = form.category.data, link = "https://open.spotify.com/embed-podcast/episode/"+get_podcast_id(form.link.data))
+            
+        else:
+            content = Content(title = form.title.data, category = form.category.data, link = form.link.data)
+            
+        db.session.add(content)
+        db.session.commit()
         flash(f'{form.title.data} successfully submitted!', 'success')
         return redirect(url_for('Home'))
     return render_template('submit_media.html', title='Submit your media', form = form)
